1a8c5d352666a9e8d534a067f8093812c94d562e
Michael Sullivan <sullivanmichael344@gmail.com>
2024-09-18 00:01:50 -0400

    Set up linkedlist

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,64 @@
 import '../css/style.css'
+
+function linkedList(){
+    let head = null;
+    let tail = null;
+    let length = 0;
+    let append = (value) =>{
+        let newNode = Node(value)
+        if (head === null){
+        head = newNode;
+        tail = newNode;
+        } else{
+            tail.nextNode = newNode
+            tail = newNode
+        }
+        length++
+    }
+    let prepend = (value) => {
+        let newNode = Node(value)
+        if (head === null){
+            head = newNode
+            tail = newNode
+        } else{
+        newNode.nextNode = head;
+        head = newNode
+        }
+        length++;
+    }
+    let size = () =>{
+        return length;
+    }
+    let getHead = () =>{
+        return head;
+    }
+    let getTail = () =>{
+        return tail;
+    }
+    let at = (index) =>{
+        let current = head
+        let count = 0
+        while(count < index){
+            current = current.nextNode
+            count++
+        }
+        return current
+    }
+
+
+    return {append, prepend, size, getHead, getTail, at}
+
+}
+
+function Node(value = null, nextNode = null){
+ 
+
+return {value, nextNode}
+}
+
+let theList = linkedList();
+theList.append("the")
+theList.append("buh")
+theList.append("aha")
+theList.prepend("first")
+console.log(theList)
